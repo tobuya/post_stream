b3e3eb75e7824aed11e6720875dc74d262b35e5d
tobuya <thomasobuya95@gmail.com>
2023-04-08 12:16:07 +0300

    Update get_recent_post method to return the 3 most recent posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   has_many :comments, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'author_id'
 
-  def get_recent_posts(limit_count = 5)
+  def get_recent_posts(limit_count = 3)
     posts.order(created_at: :desc).limit(limit_count)
   end
 end
