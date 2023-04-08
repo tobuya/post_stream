2cefa20bea57ed31f9cf5d260cfb8f6b04a5baab
tobuya <thomasobuya95@gmail.com>
2023-04-08 14:15:12 +0300

    Add validations for Name and PostsCounter

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   has_many :comments, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'author_id'
 
+  validates :name, presence: true
+  validates :post_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   def get_recent_posts(limit_count = 3)
     posts.order(created_at: :desc).limit(limit_count)
   end
