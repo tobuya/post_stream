58123c2107867355f9d82f00787c4814c1f1dffc
tobuya <thomasobuya95@gmail.com>
2023-04-08 22:55:13 +0300

    Add validations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,12 +1,12 @@
 class Comment < ApplicationRecord
-  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  belongs_to :author, class_name: 'User'
   belongs_to :post
 
   after_save :update_comments_count
 
-  private
-
-  def update_comments_count
-    post.update(comments_count: post.comments.count)
+  def update_comments_counter
+    puts "Current post comments counter: #{post.comments_counter}"
+    post.update(comments_counter: post.comments_counter + 1)
+    puts "New post comments counter: #{post.comments_counter}"
   end
 end
