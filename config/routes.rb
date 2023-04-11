becfc0ad795a961889f74a68de765e537ac899cb
tobuya <thomasobuya95@gmail.com>
2023-04-11 12:02:02 +0300

    Get posts and users views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'posts/index'
+  get 'posts/show'
+  get 'users/index'
+  get 'users/show'
   resources :users, only: %i[index show] do
     resources :posts, only: %i[index show]
   end
