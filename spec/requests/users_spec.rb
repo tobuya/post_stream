f64bc1f42a1f302dfc828a7ec34aa38dd76aa897
tobuya <thomasobuya95@gmail.com>
2023-04-12 14:28:46 +0300

    Add tests for GET/show action to check correct template and placeholder

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -24,5 +24,16 @@ RSpec.describe 'Users', type: :request do
       get '/users/show'
       expect(response).to have_http_status(:success)
     end
+
+    it 'renders the correct template' do
+      get '/users/show'
+      expect(response).to render_template(:show)
+    end
+
+    it 'response body includes the correct placeholder text' do
+      get '/users/show'
+      result = response.body
+      expect(result).to include('Here is a list of all Users')
+    end
   end
 end
