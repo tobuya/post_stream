93b0451f7a6583e4e4fbc92163d5ab3cedb80de9
tobuya <thomasobuya95@gmail.com>
2023-04-12 14:24:09 +0300

    Add tests for GET/show action to check correct template and placeholder

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -24,5 +24,16 @@ RSpec.describe 'Posts', type: :request do
       get '/posts/show'
       expect(response).to have_http_status(:success)
     end
+
+    it 'renders the correct template' do
+      get '/posts/show'
+      expect(response).to render_template(:show)
+    end
+
+    it 'response body includes the correct placeholder text' do
+      get '/posts/show'
+      result = response.body
+      expect(result).to include('Here is a list of all posts')
+    end
   end
 end
