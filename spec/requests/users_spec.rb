46965118f7b53b7aaf8493ef504bf91d23abadb6
tobuya <thomasobuya95@gmail.com>
2023-04-12 14:26:40 +0300

    Add tests for GET/index action to check correct template and placeholder

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -6,6 +6,17 @@ RSpec.describe 'Users', type: :request do
       get '/users/index'
       expect(response).to have_http_status(:success)
     end
+
+    it 'renders the correct template' do
+      get '/users/index'
+      expect(response).to render_template(:index)
+    end
+
+    it 'response body includes the correct placeholder text' do
+      get '/users/index'
+      result = response.body
+      expect(result).to include('Users by index')
+    end
   end
 
   describe 'GET /show' do
