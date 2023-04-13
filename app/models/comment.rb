198bd0122431113ca2fdb2f8376a34469d826b8e
tobuya <thomasobuya95@gmail.com>
2023-04-13 12:02:19 +0300

    Update comments counter method name

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,7 @@ class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
 
-  after_save :update_comments_count
+  after_save :update_comments_counter
 
   def update_comments_counter
     puts "Current post comments counter: #{post.comments_counter}"
