b267f9b510333cda642eccb66a97b0b53b64c695
tobuya <thomasobuya95@gmail.com>
2023-04-15 13:57:12 +0300

    Add comments and likes resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root 'users#index'
   resources :users, only: %i[index show] do
-    resources :posts, only: %i[index show]
+    resources :posts, only: %i[index show new create] do
+      resources :comments, only: %i[new create]
+      resources :likes, only: [:create]
+    end
   end
 end
