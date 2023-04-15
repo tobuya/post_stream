52e9b859bd4a525defa59171e2e3f2795807831f
tobuya <thomasobuya95@gmail.com>
2023-04-15 13:54:55 +0300

    Add new and create actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
+    @posts = Post.where(author_id: params[:user_id])
     @user_posts = Post.where(author_id: @user.id)
     @user_comments = Comment.where(user_id: @user.id)
     @post_comments = Comment.where(post_id: @user_posts.ids)
@@ -9,5 +10,26 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
     @post_comments = Comment.where(post_id: @post)
+    @like = Like.new
+  end
+
+  def new
+    @user = current_user
+    @post = Post.new
+  end
+
+  def create
+    @user = current_user
+    if params[:post].present? && params[:post][:title].present? && params[:post][:text].present?
+      @post = Post.new(author_id: @user.id, title: params[:post][:title], text: params[:post][:text])
+      if @post.save
+        redirect_to user_posts_path(@user)
+      else
+        render :new
+      end
+    else
+      flash[:error] = 'Please fill in all required fields.'
+      render :new
+    end
   end
 end
