6992c6f518b8dc494c51513da42adc84f23f2dab
tobuya <thomasobuya95@gmail.com>
2023-04-17 22:37:47 +0300

    Fix n+1 problem

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,11 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all
+    @users = User.includes(:posts).all
   end
 
   def show
-    @user = User.find(params[:id])
-    @user_posts = Post.where(author_id: @user.id)
-    @recent_posts = @user.get_recent_posts
+    @user = User.includes(posts: :comments).find(params[:id])
+    @user_posts = @user.posts
+    @recent_posts = @user.get_recent_posts.includes(:author, :comments)
   end
 end
