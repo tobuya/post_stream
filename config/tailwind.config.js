93034aaf75053dff152ce0f76ac859bd9a5d15df
tobuya <thomasobuya95@gmail.com>
2023-07-27 20:42:10 +0300

    Add the paths to all template files

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -2,10 +2,9 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
   content: [
-    './public/*.html',
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}'
+    './app/views/**/*',
   ],
   theme: {
     extend: {
